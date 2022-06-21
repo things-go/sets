71666c61a53999463528e67bc8f2ae08a66cb6d2
thinkgo <thinkgo@aliyun.com>
2022-06-21 14:27:08 +0800

    feat: 使用泛型

--- set.go
+++ set.go
@@ -1,80 +1,49 @@
 package sets
 
-import (
-	"reflect"
-)
-
 // Set sets.Set is a set of interface,
-// implemented via map[interface{}]struct{} for minimal memory consumption.
-type Set struct {
-	m   map[interface{}]struct{}
-	cmp Comparator
+// implemented via map[T]struct{} for minimal memory consumption.
+type Set[T comparable] map[T]struct{}
+
+// New creates a T from a list of values.
+func New[T comparable](items ...T) Set[T] {
+	ss := Set[T]{}
+	return ss.Insert(items...)
 }
 
-// Option option for New.
-type Option func(*Set)
-
-// WithItems with git items.
-func WithItems(items ...interface{}) Option {
-	return func(s *Set) {
-		s.Insert(items...)
-	}
-}
-
-// WithComparator with user's Comparator only for sort
-func WithComparator(cmp Comparator) Option {
-	return func(s *Set) {
-		s.cmp = cmp
-	}
-}
-
-// New creates a interface{} from a list of values.
-func New(opts ...Option) *Set {
-	ss := &Set{
-		m: make(map[interface{}]struct{}),
-	}
-	for _, opt := range opts {
-		opt(ss)
-	}
-	return ss
-}
-
-// NewSetFrom creates a interface{} from a keys of a map[interface{}](? extends interface{}).
+// NewFrom creates a T from a keys of a map[T](? extends any).
 // If the value passed in is not actually a map, this will panic.
-func NewSetFrom(theMap interface{}, opts ...Option) *Set {
-	v := reflect.ValueOf(theMap)
-	ret := New(opts...)
-
-	for _, keyValue := range v.MapKeys() {
-		ret.m[keyValue.Interface()] = struct{}{}
+func NewFrom[T comparable, M ~map[T]any](m M) Set[T] {
+	ret := Set[T]{}
+	for k := range m {
+		ret[k] = struct{}{}
 	}
 	return ret
 }
 
 // Insert adds items to the set.
-func (s *Set) Insert(items ...interface{}) *Set {
+func (s Set[T]) Insert(items ...T) Set[T] {
 	for _, item := range items {
-		s.m[item] = struct{}{}
+		s[item] = struct{}{}
 	}
 	return s
 }
 
 // Delete removes all items from the set.
-func (s *Set) Delete(items ...interface{}) *Set {
+func (s Set[T]) Delete(items ...T) Set[T] {
 	for _, item := range items {
-		delete(s.m, item)
+		delete(s, item)
 	}
 	return s
 }
 
 // Contains returns true if and only if item is contained in the set.
-func (s *Set) Contains(item interface{}) bool {
-	_, contained := s.m[item]
+func (s Set[T]) Contains(item T) bool {
+	_, contained := s[item]
 	return contained
 }
 
 // ContainsAll returns true if and only if all items are contained in the set.
-func (s *Set) ContainsAll(items ...interface{}) bool {
+func (s Set[T]) ContainsAll(items ...T) bool {
 	for _, item := range items {
 		if !s.Contains(item) {
 			return false
@@ -84,7 +53,7 @@ func (s *Set) ContainsAll(items ...interface{}) bool {
 }
 
 // ContainsAny returns true if any items are contained in the set.
-func (s *Set) ContainsAny(items ...interface{}) bool {
+func (s Set[T]) ContainsAny(items ...T) bool {
 	for _, item := range items {
 		if s.Contains(item) {
 			return true
@@ -99,11 +68,11 @@ func (s *Set) ContainsAny(items ...interface{}) bool {
 // s2 = {a1, a2, a4, a5}
 // s1.Difference(s2) = {a3}
 // s2.Difference(s1) = {a4, a5}.
-func (s *Set) Difference(s2 *Set) *Set {
-	result := New(WithComparator(s.cmp))
-	for key := range s.m {
+func (s Set[T]) Difference(s2 Set[T]) Set[T] {
+	result := New[T]()
+	for key := range s {
 		if !s2.Contains(key) {
-			result.m[key] = struct{}{}
+			result[key] = struct{}{}
 		}
 	}
 	return result
@@ -115,13 +84,13 @@ func (s *Set) Difference(s2 *Set) *Set {
 // s2 = {a3, a4}
 // s1.Union(s2) = {a1, a2, a3, a4}
 // s2.Union(s1) = {a1, a2, a3, a4}.
-func (s *Set) Union(s2 *Set) *Set {
-	result := New(WithComparator(s.cmp))
-	for key := range s.m {
-		result.m[key] = struct{}{}
+func (s Set[T]) Union(s2 Set[T]) Set[T] {
+	result := New[T]()
+	for key := range s {
+		result[key] = struct{}{}
 	}
-	for key := range s2.m {
-		result.m[key] = struct{}{}
+	for key := range s2 {
+		result[key] = struct{}{}
 	}
 	return result
 }
@@ -131,9 +100,9 @@ func (s *Set) Union(s2 *Set) *Set {
 // s1 = {a1, a2}
 // s2 = {a2, a3}
 // s1.Intersection(s2) = {a2}.
-func (s *Set) Intersection(s2 *Set) *Set {
-	var walk, other *Set
-	result := New(WithComparator(s.cmp))
+func (s Set[T]) Intersection(s2 Set[T]) Set[T] {
+	var walk, other Set[T]
+	result := New[T]()
 	if s.Len() < s2.Len() {
 		walk = s
 		other = s2
@@ -141,9 +110,9 @@ func (s *Set) Intersection(s2 *Set) *Set {
 		walk = s2
 		other = s
 	}
-	for key := range walk.m {
+	for key := range walk {
 		if other.Contains(key) {
-			result.m[key] = struct{}{}
+			result[key] = struct{}{}
 		}
 	}
 	return result
@@ -156,16 +125,16 @@ func (s *Set) Intersection(s2 *Set) *Set {
 // s2 = {a3, a4}
 // s1.Merge(s2), s1 = {a1, a2, a3, a4}
 // s2.Merge(s1), s2 = {a1, a2, a3, a4}.
-func (s *Set) Merge(s2 *Set) *Set {
-	for item := range s2.m {
-		s.m[item] = struct{}{}
+func (s Set[T]) Merge(s2 Set[T]) Set[T] {
+	for item := range s2 {
+		s[item] = struct{}{}
 	}
 	return s
 }
 
 // IsSuperset returns true if and only if s1 is a superset of s2.
-func (s *Set) IsSuperset(s2 *Set) bool {
-	for item := range s2.m {
+func (s Set[T]) IsSuperset(s2 Set[T]) bool {
+	for item := range s2 {
 		if !s.Contains(item) {
 			return false
 		}
@@ -174,8 +143,8 @@ func (s *Set) IsSuperset(s2 *Set) bool {
 }
 
 // IsSubset returns true if and only if s1 is a superset of s2.
-func (s *Set) IsSubset(s2 *Set) bool {
-	for item := range s.m {
+func (s Set[T]) IsSubset(s2 Set[T]) bool {
+	for item := range s {
 		if !s2.Contains(item) {
 			return false
 		}
@@ -184,16 +153,9 @@ func (s *Set) IsSubset(s2 *Set) bool {
 }
 
 // List returns the contents as a sorted slice.
-func (s *Set) List() []interface{} {
-	res := s.UnsortedList()
-	NewContainer(res, s.cmp).Sort()
-	return res
-}
-
-// UnsortedList returns the slice with contents in random order.
-func (s *Set) UnsortedList() []interface{} {
-	res := make([]interface{}, 0, len(s.m))
-	for key := range s.m {
+func (s Set[T]) List() []T {
+	res := make([]T, 0, len(s))
+	for key := range s {
 		res = append(res, key)
 	}
 	return res
@@ -202,29 +164,29 @@ func (s *Set) UnsortedList() []interface{} {
 // Equal returns true if and only if s1 is equal (as a set) to s2.
 // Two sets are equal if their membership is identical.
 // (In practice, this means same elements, order doesn't matter).
-func (s *Set) Equal(s2 *Set) bool {
-	return len(s.m) == len(s2.m) && s.IsSuperset(s2)
+func (s Set[T]) Equal(s2 Set[T]) bool {
+	return len(s) == len(s2) && s.IsSuperset(s2)
 }
 
 // Pop Returns a single element from the set.
-func (s *Set) Pop() (interface{}, bool) {
-	for key := range s.m {
-		delete(s.m, key)
+func (s Set[T]) Pop() (v T, ok bool) {
+	for key := range s {
+		delete(s, key)
 		return key, true
 	}
-	return nil, false
+	return
 }
 
 // Len returns the size of the set.
-func (s *Set) Len() int {
-	return len(s.m)
+func (s Set[T]) Len() int {
+	return len(s)
 }
 
 // Each traverses the items in the Set, calling the provided function for each
 // set member. Traversal will continue until all items in the Set have been
 // visited, or if the closure returns false.
-func (s *Set) Each(f func(item interface{}) bool) {
-	for item := range s.m {
+func (s Set[T]) Each(f func(item T) bool) {
+	for item := range s {
 		if !f(item) {
 			break
 		}
@@ -232,10 +194,10 @@ func (s *Set) Each(f func(item interface{}) bool) {
 }
 
 // Clone returns a new Set with a copy of s.
-func (s *Set) Clone() *Set {
-	ns := New(WithComparator(s.cmp))
-	s.Each(func(item interface{}) bool {
-		ns.m[item] = struct{}{}
+func (s Set[T]) Clone() Set[T] {
+	ns := New[T]()
+	s.Each(func(item T) bool {
+		ns[item] = struct{}{}
 		return true
 	})
 	return ns
--- set_test.go
+++ set_test.go
@@ -17,206 +17,190 @@ limitations under the License.
 package sets
 
 import (
-	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestSet(t *testing.T) {
-	s := New()
-	s2 := New()
-	if len(s.m) != 0 {
-		t.Errorf("Expected len=0: %d", len(s.m))
+	s := Set[int]{}
+	s2 := Set[int]{}
+	if len(s) != 0 {
+		t.Errorf("Expected len=0: %d", len(s))
 	}
-	s.Insert("a", "b")
-	if len(s.m) != 2 {
-		t.Errorf("Expected len=2: %d", len(s.m))
+	s.Insert(1, 2)
+	if len(s) != 2 {
+		t.Errorf("Expected len=2: %d", len(s))
 	}
-	s.Insert("c")
-	if s.Contains("d") {
+	s.Insert(3)
+	if s.Contains(4) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
-	if !s.Contains("a") {
+	if !s.Contains(1) {
 		t.Errorf("Missing contents: %#v", s)
 	}
-	s.Delete("a")
-	if s.Contains("a") {
+	s.Delete(1)
+	if s.Contains(1) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
-	s.Insert("a")
-	if s.ContainsAll("a", "b", "d") {
+	s.Insert(1)
+	if s.ContainsAll(1, 2, 4) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
-	if !s.ContainsAll("a", "b") {
+	if !s.ContainsAll(1, 2) {
 		t.Errorf("Missing contents: %#v", s)
 	}
-	s2.Insert("a", "b", "d")
+	s2.Insert(1, 2, 4)
 	if s.IsSuperset(s2) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
 	if s2.IsSubset(s) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
-	s2.Delete("d")
+	s2.Delete(4)
 	if !s.IsSuperset(s2) {
 		t.Errorf("Missing contents: %#v", s)
 	}
 	if !s2.IsSubset(s) {
-		t.Errorf("Unexpected contents: %#v", s)
+		t.Errorf("Missing contents: %#v", s)
 	}
 	_, ok := s2.Pop()
 	if !ok {
 		t.Errorf("Unexpected status: %#v", ok)
 	}
-	s2 = New()
+	s2 = New[int]()
 	if s2.Len() != 0 {
-		t.Errorf("Expected len=0: %d", s2.Len())
+		t.Errorf("Expected len=0: %d", len(s2))
 	}
 	v, ok := s2.Pop()
 	if ok {
 		t.Errorf("Unexpected status: %#v", ok)
 	}
-	if v != nil {
+	if v != 0 {
 		t.Errorf("Unexpected value: %#v", v)
 	}
-	// improve cover
-	s2 = NewSetFrom(map[interface{}]interface{}{1: "1", 2: "2", 3: "3"})
-	s2.UnsortedList()
-
-	s3 := New(WithComparator(CompareMyInt))
-	s3.Insert([]interface{}{15, 19, 12, 8, 13}...)
-
-	lists := s3.List()
-	if !reflect.DeepEqual(lists, []interface{}{8, 12, 13, 15, 19}) {
-		t.Errorf("Unexpected list: %#v", lists)
-	}
-}
-
-// Compare returns reverse order.
-func CompareMyInt(v1, v2 interface{}) int {
-	i1, i2 := v1.(int), v2.(int)
-	if i1 < i2 {
-		return -1
-	}
-	if i1 > i2 {
-		return 1
-	}
-	return 0
 }
 
 func TestSetDeleteMultiples(t *testing.T) {
-	s := New()
-	s.Insert("a", "b", "c")
-	if len(s.m) != 3 {
-		t.Errorf("Expected len=3: %d", len(s.m))
+	s := NewFrom(map[int]any{1: "1", 2: "2", 3: "3"})
+	if len(s) != 3 {
+		t.Errorf("Expected len=3: %d", len(s))
 	}
 
-	s.Delete("a", "c")
-	if len(s.m) != 1 {
-		t.Errorf("Expected len=1: %d", len(s.m))
+	s.Delete(1, 3)
+	if len(s) != 1 {
+		t.Errorf("Expected len=1: %d", len(s))
 	}
-	if s.Contains("a") {
+	if s.Contains(1) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
-	if s.Contains("c") {
+	if s.Contains(3) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
-	if !s.Contains("b") {
+	if !s.Contains(2) {
 		t.Errorf("Missing contents: %#v", s)
 	}
 }
 
 func TestNewSet(t *testing.T) {
-	s := New(WithItems("a", "b", "c"))
-	if len(s.m) != 3 {
-		t.Errorf("Expected len=3: %d", len(s.m))
+	s := New(1, 2, 3)
+	if len(s) != 3 {
+		t.Errorf("Expected len=3: %d", len(s))
 	}
-	if !s.Contains("a") || !s.Contains("b") || !s.Contains("c") {
+	if !s.Contains(1) || !s.Contains(2) || !s.Contains(3) {
 		t.Errorf("Unexpected contents: %#v", s)
 	}
 }
 
+func TestSetList(t *testing.T) {
+	s := New(13, 12, 1, 11)
+	v := s.List()
+
+	for _, vv := range v {
+		if !s.Contains(vv) {
+			t.Errorf("List gave unexpected result: %#v", v)
+		}
+	}
+}
+
 func TestSetDifference(t *testing.T) {
-	a := New(WithItems("1", "2", "3"))
-	b := New(WithItems("1", "2", "4", "5"))
+	a := New(1, 2, 3)
+	b := New(1, 2, 4, 5)
 	c := a.Difference(b)
 	d := b.Difference(a)
-	if len(c.m) != 1 {
-		t.Errorf("Expected len=1: %d", len(c.m))
+	if len(c) != 1 {
+		t.Errorf("Expected len=1: %d", len(c))
 	}
-	if !c.Contains("3") {
+	if !c.Contains(3) {
 		t.Errorf("Unexpected contents: %#v", c.List())
 	}
-	if len(d.m) != 2 {
-		t.Errorf("Expected len=2: %d", len(d.m))
+	if len(d) != 2 {
+		t.Errorf("Expected len=2: %d", len(d))
 	}
-	if !d.Contains("4") || !d.Contains("5") {
+	if !d.Contains(4) || !d.Contains(5) {
 		t.Errorf("Unexpected contents: %#v", d.List())
 	}
 }
 
 func TestSetHasAny(t *testing.T) {
-	a := New(WithItems("1", "2", "3"))
+	a := New(1, 2, 3)
 
-	if !a.ContainsAny("1", "4") {
+	if !a.ContainsAny(1, 4) {
 		t.Errorf("expected true, got false")
 	}
 
-	if a.ContainsAny("0", "4") {
+	if a.ContainsAny(10, 4) {
 		t.Errorf("expected false, got true")
 	}
 }
 
 func TestSetEquals(t *testing.T) {
 	// Simple case (order doesn't matter)
-	a := New(WithItems("1", "2"))
-	b := New(WithItems("2", "1"))
+	a := New[int](1, 2)
+	b := New[int](2, 1)
 	if !a.Equal(b) {
 		t.Errorf("Expected to be equal: %v vs %v", a, b)
 	}
 
 	// It is a set; duplicates are ignored
-	b = New(WithItems("2", "2", "1"))
+	b = New[int](2, 2, 1)
 	if !a.Equal(b) {
 		t.Errorf("Expected to be equal: %v vs %v", a, b)
 	}
 
 	// Edge cases around empty sets / empty strings
-	a = New()
-	b = New()
+	a = New[int]()
+	b = New[int]()
 	if !a.Equal(b) {
 		t.Errorf("Expected to be equal: %v vs %v", a, b)
 	}
 
-	b = New(WithItems("1", "2", "3"))
+	b = New(1, 2, 3)
 	if a.Equal(b) {
 		t.Errorf("Expected to be not-equal: %v vs %v", a, b)
 	}
 
-	b = New(WithItems("1", "2", ""))
+	b = New(1, 2, 0)
 	if a.Equal(b) {
 		t.Errorf("Expected to be not-equal: %v vs %v", a, b)
 	}
 
 	// Check for equality after mutation
-	a = New()
-	a.Insert("1")
+	a = New[int]()
+	a.Insert(1)
 	if a.Equal(b) {
 		t.Errorf("Expected to be not-equal: %v vs %v", a, b)
 	}
 
-	a.Insert("2")
+	a.Insert(2)
 	if a.Equal(b) {
 		t.Errorf("Expected to be not-equal: %v vs %v", a, b)
 	}
 
-	a.Insert("")
+	a.Insert(0)
 	if !a.Equal(b) {
 		t.Errorf("Expected to be equal: %v vs %v", a, b)
 	}
 
-	a.Delete("")
+	a.Delete(0)
 	if a.Equal(b) {
 		t.Errorf("Expected to be not-equal: %v vs %v", a, b)
 	}
@@ -224,29 +208,29 @@ func TestSetEquals(t *testing.T) {
 
 func TestUnion(t *testing.T) {
 	tests := []struct {
-		s1       *Set
-		s2       *Set
-		expected *Set
+		s1       Set[int]
+		s2       Set[int]
+		expected Set[int]
 	}{
 		{
-			New(WithItems("1", "2", "3", "4")),
-			New(WithItems("3", "4", "5", "6")),
-			New(WithItems("1", "2", "3", "4", "5", "6")),
+			New[int](1, 2, 3, 4),
+			New[int](3, 4, 5, 6),
+			New[int](1, 2, 3, 4, 5, 6),
 		},
 		{
-			New(WithItems("1", "2", "3", "4")),
-			New(),
-			New(WithItems("1", "2", "3", "4")),
+			New[int](1, 2, 3, 4),
+			New[int](),
+			New[int](1, 2, 3, 4),
 		},
 		{
-			New(),
-			New(WithItems("1", "2", "3", "4")),
-			New(WithItems("1", "2", "3", "4")),
+			New[int](),
+			New[int](1, 2, 3, 4),
+			New[int](1, 2, 3, 4),
 		},
 		{
-			New(),
-			New(),
-			New(),
+			New[int](),
+			New[int](),
+			New[int](),
 		},
 	}
 
@@ -264,34 +248,34 @@ func TestUnion(t *testing.T) {
 
 func TestIntersection(t *testing.T) {
 	tests := []struct {
-		s1       *Set
-		s2       *Set
-		expected *Set
+		s1       Set[int]
+		s2       Set[int]
+		expected Set[int]
 	}{
 		{
-			New(WithItems("1", "2", "3", "4")),
-			New(WithItems("3", "4", "5", "6")),
-			New(WithItems("3", "4")),
+			New[int](1, 2, 3, 4),
+			New[int](3, 4, 5, 6),
+			New[int](3, 4),
 		},
 		{
-			New(WithItems("1", "2", "3", "4")),
-			New(WithItems("1", "2", "3", "4")),
-			New(WithItems("1", "2", "3", "4")),
+			New[int](1, 2, 3, 4),
+			New[int](1, 2, 3, 4),
+			New[int](1, 2, 3, 4),
 		},
 		{
-			New(WithItems("1", "2", "3", "4")),
-			New(),
-			New(),
+			New[int](1, 2, 3, 4),
+			New[int](),
+			New[int](),
 		},
 		{
-			New(),
-			New(WithItems("1", "2", "3", "4")),
-			New(),
+			New[int](),
+			New[int](1, 2, 3, 4),
+			New[int](),
 		},
 		{
-			New(),
-			New(),
-			New(),
+			New[int](),
+			New[int](),
+			New[int](),
 		},
 	}
 
@@ -310,56 +294,65 @@ func TestIntersection(t *testing.T) {
 
 func TestMerge(t *testing.T) {
 	tests := []struct {
-		s1       *Set
-		s2       *Set
-		expected *Set
+		s1       Set[int]
+		s2       Set[int]
+		expected Set[int]
 	}{
 		{
-			New(WithItems("1", "2", "3", "4")),
-			New(WithItems("3", "4", "5", "6")),
-			New(WithItems("1", "2", "3", "4", "5", "6")),
+			New[int](1, 2, 3, 4),
+			New[int](3, 4, 5, 6),
+			New[int](1, 2, 3, 4, 5, 6),
 		},
 		{
-			New(WithItems("1", "2", "3", "4")),
-			New(),
-			New(WithItems("1", "2", "3", "4")),
+			New[int](1, 2, 3, 4),
+			New[int](1, 2, 3, 4),
+			New[int](1, 2, 3, 4),
 		},
 		{
-			New(),
-			New(WithItems("1", "2", "3", "4")),
-			New(WithItems("1", "2", "3", "4")),
+			New[int](1, 2, 3, 4),
+			New[int](),
+			New[int](1, 2, 3, 4),
 		},
 		{
-			New(),
-			New(),
-			New(),
+			New[int](),
+			New[int](1, 2, 3, 4),
+			New[int](1, 2, 3, 4),
+		},
+		{
+			New[int](),
+			New[int](),
+			New[int](),
 		},
 	}
 
 	for _, test := range tests {
-		test.s1.Merge(test.s2)
-		if test.s1.Len() != test.expected.Len() {
-			t.Errorf("Expected union.Len()=%d but got %d", test.expected.Len(), test.s1.Len())
+		intersection := test.s1.Merge(test.s2)
+		if intersection.Len() != test.expected.Len() {
+			t.Errorf("Expected merge.Len()=%d but got %d", test.expected.Len(), intersection.Len())
 		}
 
-		if !test.s1.Equal(test.expected) {
-			t.Errorf("Expected union.Equal(expected) but not true.  union:%v expected:%v", test.s1.List(), test.expected.List())
+		if !intersection.Equal(test.expected) {
+			t.Errorf("Expected merge.Equal(expected) but not true.  merge:%v expected:%v",
+				intersection.List(), test.expected.List())
 		}
 	}
 }
 
-func TestSet_Each(t *testing.T) {
-	expect := New(WithItems("1", "2", "3", "4"))
-	s1 := New(WithItems("1", "2", "3", "4"))
-	s1.Each(func(item interface{}) bool {
-		require.True(t, expect.Contains(item))
-		return item.(string) != "3"
+func Test_Each(t *testing.T) {
+	expect := New(1, 2, 3, 4)
+	s1 := New(1, 2, 3, 4)
+	s1.Each(func(item int) bool {
+		if got := expect.Contains(item); !got {
+			t.Errorf("Expected Equal()=%v but got %v", true, got)
+		}
+		return item != 3
 	})
 }
 
-func TestSet_Clone(t *testing.T) {
-	s1 := New(WithItems("1", "2", "3", "4"))
+func Test_Clone(t *testing.T) {
+	s1 := New(1, 2, 3, 4)
 	s2 := s1.Clone()
-
-	require.True(t, s1.Equal(s2))
+	if got := s1.Equal(s2); !got {
+		t.Errorf("Expected Equal()=%v but got %v", true, got)
+	}
 }
